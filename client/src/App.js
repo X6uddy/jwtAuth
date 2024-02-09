a5c81df34783c4f9bf593b850bef6bcea2af8370
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-09 18:37:53 +0400

    Creating the main structure of the project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <p>Hello JWT</p>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+const URL = process.env.DB_URL;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors())
+
+const start = async () => {
+    try {
+        await mongoose.connect(URL)
+        .then(() => console.log('Connected to DB'))
+
+        app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+start();
+
