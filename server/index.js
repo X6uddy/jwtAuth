3f2040730f5c65ea6db98d6d85f41939b508110b
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-13 22:44:22 +0400

    minor changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5000;
 const URL = process.env.DB_URL;
@@ -10,7 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
     try {
