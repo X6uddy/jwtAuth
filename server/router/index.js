1f04b3e2f7f243d1ceb8be20b1510998041957e3
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-17 14:24:32 +0400

    added validation of input data

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,13 @@
 const Router = require('express').Router;
+const {body} = require('express-validator');
 const UserController = require('../controllers/user-controller');
 
 const router = new Router();
 
-router.post('/registration', UserController.registration);
+router.post('/registration', 
+            body('email').isEmail(),
+            body('password').isLength({min: 3, max: 20}),
+            UserController.registration);
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
 router.get('/activate/:link', UserController.activate);
