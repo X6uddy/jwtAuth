9f877e5e1938e44f31df828fde338de710b97b14
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-19 17:30:23 +0400

    added middleware for  route '/users'

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router;
 const {body} = require('express-validator');
 const UserController = require('../controllers/user-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 const router = new Router();
 
@@ -12,6 +13,6 @@ router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
 router.get('/activate/:link', UserController.activate);
 router.get('/refresh', UserController.refresh);
-router.get('/users', UserController.users);
+router.get('/users', authMiddleware, UserController.users);
 
 module.exports = router;
