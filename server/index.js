093566183ac9ea28c5d679f0a512c438c489d47f
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-21 21:42:36 +0400

    added options in cors

--- server/index.js
+++ server/index.js
@@ -10,9 +10,21 @@ const PORT = process.env.PORT || 5000;
 const URL = process.env.DB_URL;
 const app = express();
 
+// app.use(express.json());
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", '*');
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use('/api', router);
 app.use(errorMiddleware)
 
