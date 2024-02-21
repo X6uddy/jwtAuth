30f8f9683498a40fda9173fd0a7f00b238ea862b
Volkov Sasha <sashavolkov_03@mail.ru>
2024-02-21 21:41:23 +0400

    The login and registration form has been added

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '../index';
+import { observer } from 'mobx-react-lite';
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const {store} = useContext(Context);
+
+    return (
+        <div>
+            <input 
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                type="text" 
+                placeholder='email' 
+            />
+            
+            <input 
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                type="password"  
+                placeholder='password'
+            />
+            <button onClick={() => store.login(email, password)}>Логин</button>
+            <button onClick={() => store.registration(email, password)}>Ругистрация</button>
+        </div>
+    );
+};
+
+export default observer(LoginForm);
